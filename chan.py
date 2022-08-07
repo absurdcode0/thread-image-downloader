f9cd6b12e1bf9039eda7352582f66eb8edfdc8b3
absurdcode0 <83966219+absurdcode0@users.noreply.github.com>
2022-08-07 17:04:37 +0300

    Update chan.py

--- chan.py
+++ chan.py
@@ -1,29 +1,30 @@
-import requests, os, sys
+from sys import argv
+from requests import get
+from os import chdir, makedirs, path
 
-
-url = sys.argv[1]
+url = argv[1]
 url = url.split("/") #['https:', '', 'boards.4channel.org', 'g', 'thread', '111111']
 board = url[3]
 thread_id = url[-1]
 
 #workpath = os.path.dirname(os.path.realpath(__file__))
 
-r = requests.get(f'https://a.4cdn.org/{board}/thread/{thread_id}.json')
+r = get(f'https://a.4cdn.org/{board}/thread/{thread_id}.json')
 r = r.json()
 
 
 def board_folder(folder_title):
-        if os.path.isdir(folder_title):
-            os.chdir(rf"{folder_title}")
+        if path.isdir(folder_title):
+            chdir(rf"{folder_title}")
             thread_folder(thread_id)
         else:
-            os.makedirs(folder_title)
-            os.chdir(rf"{folder_title}")
+            makedirs(folder_title)
+            chdir(rf"{folder_title}")
             thread_folder(thread_id)
 
 def thread_folder(postno):
-    os.makedirs(postno)
-    os.chdir(rf"{postno}")
+    makedirs(postno)
+    chdir(rf"{postno}")
 def main():
     for item in r['posts']:
         try:
@@ -32,7 +33,7 @@ def main():
             full = f"{id}{ext}"
             image_url = f'https://is2.4chan.org/{board}/{id}{ext}'
                     
-            img_data = requests.get(image_url).content
+            img_data = get(image_url).content
 
             with open(full, 'wb') as handler:
                 handler.write(img_data)
