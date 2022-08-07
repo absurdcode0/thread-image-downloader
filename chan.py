c11587508a753b831dd799b8e8332683642547c5
absurdcode0 <83966219+absurdcode0@users.noreply.github.com>
2022-08-07 14:54:53 +0300

    Update chan.py

--- chan.py
+++ chan.py
@@ -1,43 +1,31 @@
-import requests, os, re
+import requests, os, sys
 
-board = ''           # Enter your board e.g adv, g, a etc
-post_id = ''         # Enter Post ID (Number next to the Time)
+
+url = sys.argv[1]
+url = url.split("/") #['https:', '', 'boards.4channel.org', 'g', 'thread', '111111']
+board = url[3]
+thread_id = url[-1]
 
 # Enter where you want to download
-path = os.path.join(os.sep, 'C:', os.sep, 'Path', 'To', 'X',)  # Windows Example prints C:\Users\Path\To\X
-# Linux Example prints /home/name/documents, just an exmaple edit this ^
-dir_lin = os.path.join(os.sep, 'home', 'name', 'Documents')             
+path = os.path.join(os.sep, 'C:', os.sep, 'Path', 'To', 'X')  # Windows Example  C:\Users\Path\To\X
+dir_lin = os.path.join(os.sep, 'home', 'name', 'Documents')             # Linux Example prints /home/name/documents
 
-r = requests.get(f'https://a.4cdn.org/{board}/thread/{post_id}.json')
+r = requests.get(f'https://a.4cdn.org/{board}/thread/{thread_id}.json')
 r = r.json()
 
-def get_title():
-    try:
-        for x in r['posts']:
-            title = x['sub']
-            break
-        title = re.sub(r'[^\w]', ' ', title)
-        title = title.strip()
-        titled_folder(title)
-    except KeyError:
-        board_folder(post_id)
-def board_folder(folder_title):
-        new_dir = folder_title
-        fullname = f"{board}-{new_dir}"
-        if os.path.isdir(new_dir):
-            os.makedirs(fullname)
-            os.chdir(rf"{path}{os.sep}{fullname}")
-        else:
-            os.makedirs(fullname)
-            os.chdir(rf"{path}{os.sep}{fullname}")
-def titled_folder(folder):
-    if os.path.isdir(folder):
-        os.makedirs(rf"{path}{os.sep}{folder}-{post_id}")
-        os.chdir(rf"{path}{os.sep}{folder}-{post_id}")
-    else:
-        os.makedirs(rf"{path}{os.sep}{folder}")
-        os.chdir(rf"{path}{os.sep}{folder}")
 
+def board_folder(folder_title):
+        if os.path.isdir(folder_title):
+            os.chdir(rf"{folder_title}")
+            thread_folder(thread_id)
+        else:
+            os.makedirs(folder_title)
+            os.chdir(rf"{folder_title}")
+            thread_folder(thread_id)
+
+def thread_folder(postno):
+    os.makedirs(postno)
+    os.chdir(rf"{postno}")
 def main():
     for item in r['posts']:
         try:
@@ -51,7 +39,7 @@ def main():
             with open(full, 'wb') as handler:
                 handler.write(img_data)
                 print("image downloaded")
-        except KeyError:
+        except KeyError: # if no image
             continue        
-get_title()
+board_folder(board)
 main()
